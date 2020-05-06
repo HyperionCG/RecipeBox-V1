577fe0251a597d9aca14f4f63bf629d23b71b256
Babatunde Odumosu <tudy4300@pop-os.localdomain>
2020-05-06 00:21:30 -0500

    final commit

--- recipes/urls.py
+++ recipes/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from recipes import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index),
+    path('author/<int:id>', views.author),
+    path('recipe/<int:id>', views.recipe)
     #path('admin/', admin.site.urls),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 
-from recipes.models import Recipe
+from recipes.models import Author, Recipe
 
 # Create your views here.
 def index(request):
     data = Recipe.objects.all()
     return render(request, 'index.html', {'data': data})
+
+def recipe(request, id):
+    data = Recipe.objects.get(id=id)
+    return render(request, 'recipespage.html', {'data': data})
+
+def author(request, id):
+    namedata = Author.objects.get(id=id)
+    data = Recipe.objects.filter(author=namedata)
+    return render(request, 'authorpage.html',
+                                        {'namedata': namedata, 'data': data})
