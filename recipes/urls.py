09d235ffa09d8042d3f69585cbeb0027215e0b1c
Babatunde Odumosu <tudy4300@pop-os.localdomain>
2020-05-08 09:17:13 -0500

    fixed sytax error

--- recipes/urls.py
+++ recipes/urls.py
@@ -3,8 +3,10 @@ from django.urls import path
 from recipes import views
 
 urlpatterns = [
-    path('', views.index),
-    path('author/<int:id>', views.author),
-    path('recipe/<int:id>', views.recipe)
+    path('', views.index, name="homepage"),
+    path('addrecipe/', views.add_recipe),
+    path('addauthor/', views.add_author),
+    path('author/<int:id>/', views.author),
+    path('recipe/<int:id>/', views.recipe)
     #path('admin/', admin.site.urls),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,11 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse, HttpResponseRedirect
 
 from recipes.models import Author, Recipe
+from recipes.forms import AddRecipeForm, AddAuthorForm
 
 # Create your views here.
 def index(request):
     data = Recipe.objects.all()
     return render(request, 'index.html', {'data': data})
+
+def add_author(request):
+    html = "generic_form.html"
+    
+    if request.method == "POST":
+        form = AddAuthorForm(request.POST)
+        form.save()
+        return HttpResponseRedirect(reverse('homepage'))
+    
+    form = AddAuthorForm()
+
+    return render(request, html, {"form": form})
+
+def add_recipe(request):
+    html = "generic_form.html"
+    
+    if request.method == "POST":
+        form = AddRecipeForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            Recipe.objects.create(
+                title=data['title'],
+                author=data['author'],
+                description=data['description'],
+                time_required=data['time_required'],
+                instructions=data['instructions']
+
+            )
+            return HttpResponseRedirect(reverse('homepage'))
+    
+    form = AddRecipeForm()
+
+    return render(request, html, {"form": form})
 
 def recipe(request, id):
     data = Recipe.objects.get(id=id)
